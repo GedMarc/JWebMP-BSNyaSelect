9d60015fc96fc10f34d06bb087bbab4ce7ccaa39
Marc Magon <ged_marc@hotmail.com>
2017-03-04 09:24:23 +0200

    v0.9

--- src/za/co/mmagon/jwebswing/angularnyabootstrapselector/NysSelectBootstrapConfigurator.java
+++ src/za/co/mmagon/jwebswing/angularnyabootstrapselector/NysSelectBootstrapConfigurator.java
@@ -1,7 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package za.co.mmagon.jwebswing.angularnyabootstrapselector;
 
@@ -16,12 +27,22 @@ import za.co.mmagon.jwebswing.plugins.bootstrap.BootstrapPageConfigurator;
  * @since 22 Feb 2017
  *
  */
-@PluginInformation(pluginName = "Nya Bootstrap Select", pluginUniqueName = "bootstrap-nya-select",
-        pluginDescription = "nya-bootstrap-select helps you build select element or dropdown with Bootstrap style and an angular way. Generate options with collection, get user select value with model. Define various styles of the select options as you wish. ",
-        pluginVersion = "Bootstrap version 3 or 4",
-        pluginDependancyUniqueIDs = "jquery,bootstrap,angular", pluginCategories = "bootstrap,web ui,ui,framework,select", pluginSubtitle = "An Bootstrap dropdown component and replacement of vanilla select element, designed for AngularJS",
-        pluginGitUrl = "https://github.com/GedMarc/JWebSwing-BSNyaSelect", pluginSourceUrl = "http://nya.io/nya-bootstrap-select/#!/",
-        pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-BSNyaSelect/wiki")
+@PluginInformation(pluginName = "Nya Bootstrap Select",
+                   pluginUniqueName = "bootstrap-nya-select",
+                   pluginDescription = "nya-bootstrap-select helps you build select element or dropdown with Bootstrap style and an angular way. Generate options with collection, get user select value with model. Define various styles of the select options as you wish. ",
+                   pluginVersion = "BS3.3.7 / 4a6",
+                   pluginDependancyUniqueIDs = "jquery,bootstrap,angular",
+                   pluginCategories = "bootstrap,web ui,ui,framework,select",
+                   pluginSubtitle = "An Bootstrap dropdown component and replacement of vanilla select element, designed for AngularJS",
+                   pluginGitUrl = "https://github.com/GedMarc/JWebSwing-BSNyaSelect",
+                   pluginSourceUrl = "https://github.com/lordfriend/nya-bootstrap-select",
+                   pluginWikiUrl = "https://github.com/GedMarc/JWebSwing-BSNyaSelect/wiki",
+                   pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/BSNyaSelect.jar/download",
+                   pluginIconUrl = "bower_components/nya-bootstrap-select/logo.png",
+                   pluginIconImageUrl = "bower_components/nya-bootstrap-select/logo.png",
+                   pluginLastUpdatedDate = "2017/03/04",
+                   pluginOriginalHomepage = "http://nya.io/nya-bootstrap-select/#!/"
+)
 public class NysSelectBootstrapConfigurator extends PageConfigurator
 {
 
